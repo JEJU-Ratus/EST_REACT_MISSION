c94a68542bcd8406257bbe589ceb21aebc8e4deb
jema <smh1141@naver.com>
2026-06-22 16:20:29 +0900

    feat : props 과제

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CardList from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <CardList name="무선 키보드" price="39000" brand="EZTECH" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function CardList(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.price}</p>
+      <p>{props.brand}</p>
+    </div>
+  );
+}
